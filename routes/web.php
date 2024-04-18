7af71903ae41d1725b8dd81909a65ece8f3e2761
douglas.arruda@fatec.sp.gov.br <douglas.arruda@fatec.sp.gov.br>
2024-04-18 10:44:14 -0300

    Pagina do login

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
 
 
-Route::view('/home','home');
+Route::view('/','home');
+
+Route::view('/cria-conta','cria-conta');
 
 Route::view('/teste de conteudo','teste');
