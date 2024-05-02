30d52e85a82e774f3be09c0de58195e0760b0595
DouglasReis1967 <douglas.arruda@fatec.sp.gov.br>
2024-05-02 10:48:37 -0300

    Comando inia aula e salva banco de dados

--- resources/views/cria-conta.blade.php
+++ resources/views/cria-conta.blade.php
@@ -30,7 +30,8 @@
 
       <div>
         <!-- Form -->
-        <form>
+        <form action="{{route('salva-usuario')}}"method="POST">
+          @csrf
           <div class="lg:max-w-lg lg:mx-auto lg:me-0 ms-auto">
             <!-- Card -->
             <div class="p-4 sm:p-7 flex flex-col bg-white rounded-2xl shadow-lg dark:bg-neutral-900">
@@ -63,7 +64,7 @@
                   <div>
                     <!-- Floating Input -->
                     <div class="relative">
-                      <input type="text" id="hs-hero-signup-form-floating-input-first-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                      <input type="text" name ="nome" id="hs-hero-signup-form-floating-input-first-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
                       focus:pt-6
                       focus:pb-2
                       [&:not(:placeholder-shown)]:pt-6
@@ -88,7 +89,7 @@
                   <div>
                     <!-- Floating Input -->
                     <div class="relative">
-                      <input type="text" id="hs-hero-signup-form-floating-input-last-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                      <input type="text" name="nome" id="hs-hero-signup-form-floating-input-last-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
                       focus:pt-6
                       focus:pb-2
                       [&:not(:placeholder-shown)]:pt-6
@@ -113,7 +114,7 @@
                   <div>
                     <!-- Floating Input -->
                     <div class="relative">
-                      <input type="email" id="hs-hero-signup-form-floating-input-email" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                      <input type="email" name="email" id="hs-hero-signup-form-floating-input-email" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
                       focus:pt-6
                       focus:pb-2
                       [&:not(:placeholder-shown)]:pt-6
@@ -135,35 +136,14 @@
                   <!-- End Input Group -->
 
                   <!-- Input Group -->
-                  <div>
-                    <!-- Floating Input -->
-                    <div class="relative">
-                      <input type="text" id="hs-hero-signup-form-floating-input-company-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
-                      focus:pt-6
-                      focus:pb-2
-                      [&:not(:placeholder-shown)]:pt-6
-                      [&:not(:placeholder-shown)]:pb-2
-                      autofill:pt-6
-                      autofill:pb-2" placeholder="Preline">
-                      <label for="hs-hero-signup-form-floating-input-company-name" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
-                        peer-focus:scale-90
-                        peer-focus:translate-x-0.5
-                        peer-focus:-translate-y-1.5
-                        peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
-                        peer-[:not(:placeholder-shown)]:scale-90
-                        peer-[:not(:placeholder-shown)]:translate-x-0.5
-                        peer-[:not(:placeholder-shown)]:-translate-y-1.5
-                        peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 dark:text-neutral-500">Company name</label>
-                    </div>
-                    <!-- End Floating Input -->
-                  </div>
+                  
                   <!-- End Input Group -->
 
                   <!-- Input Group -->
                   <div class="relative col-span-full">
                     <!-- Floating Input -->
                     <div class="relative">
-                      <input type="password" id="hs-hero-signup-form-floating-input-new-password" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                      <input type="password" name="senha" id="hs-hero-signup-form-floating-input-new-password" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
                       focus:pt-6
                       focus:pb-2
                       [&:not(:placeholder-shown)]:pt-6
@@ -250,7 +230,7 @@
                   <div class="col-span-full">
                     <!-- Floating Input -->
                     <div class="relative">
-                      <input type="password" id="hs-hero-signup-form-floating-input-current-password" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                      <input type="password" name="senha" id="hs-hero-signup-form-floating-input-current-password" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
                       focus:pt-6
                       focus:pb-2
                       [&:not(:placeholder-shown)]:pt-6
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use App\Models\User;
 Route::get('/home', function () {
     return view('welcome');
 });
@@ -12,3 +13,14 @@ Route::view('/','home');
 Route::view('/cria-conta','cria-conta');
 
 Route::view('/teste de conteudo','teste');
+
+Route::post('/salva-usuario', function (Request $request) {
+    //dd($request);
+    $usuario=new User();
+    $usuario->name = $request->nome;
+    $usuario->email = $request->email;
+    $usuario->password = $request->senha;
+    $usuario->save();
+    dd("salvo com sucesso!!");
+
+})->name('salva-usuario');
